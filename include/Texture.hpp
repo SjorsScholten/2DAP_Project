ecfdacc26179f1ee5dfec3e343a577381493e883
Sjors Scholten <scholtensjors22@outlook.com>
2022-02-12 00:11:36 +0100

    texture an shader

---
+++ include/Texture.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+class Texture2D {
+public:
+    unsigned int id;
+    unsigned int width, height;
+    unsigned int internalFormat;
+    unsigned int imageFormat;
+    unsigned int wrapS;
+    unsigned int wrapT;
+    unsigned int filterMin;
+    unsigned int filterMax;
+
+    Texture2D();
+
+    void Generate(unsigned int width, unsigned int height, unsigned char *data);
+
+    void Bind() const;
+};
